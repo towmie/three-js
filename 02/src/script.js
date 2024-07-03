6cdf5af3a7a77b34c2238e3141a8ce0b23b2d3f1
towmie <andre00toz@gmail.com>
2024-07-03 14:59:23 +0300

    feat: screen resizing

--- 02/src/script.js
+++ 02/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -29,15 +30,22 @@ gsap.to(mesh.position, {
  * Sizes
  */
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 3;
+camera.position.y = 1.25;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 /**
@@ -47,16 +55,56 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 let time = Date.now();
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+camera.lookAt(mesh.position);
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
 
 const tick = () => {
   const currentTime = Date.now();
   const deltaTime = currentTime - time;
   time = currentTime;
 
+  // Update controls
+  controls.update();
+
   // Update objects
-  mesh.rotation.y += 0.001 * deltaTime;
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
