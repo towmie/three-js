f81e881adcc7ade0e0020445fdac6fc23bf16568
towmie <andre00toz@gmail.com>
2024-07-22 20:09:12 +0300

    r3f: add lights

--- 20-portal-r3f/src/Experience.jsx
+++ 20-portal-r3f/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import { Center, OrbitControls, useGLTF, useTexture } from "@react-three/drei";
 
 export default function Experience() {
   const { nodes } = useGLTF("./model/portal.glb");
@@ -10,10 +10,31 @@ export default function Experience() {
       <color args={["#030202"]} attach="background" />
 
       <OrbitControls makeDefault />
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={texture} />
+        </mesh>
 
-      <mesh geometry={nodes.baked.geometry}>
-        <meshBasicMaterial map={texture} />
-      </mesh>
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+          rotation={nodes.poleLightA.rotation}
+          scale={nodes.poleLightA.scale}
+        ></mesh>
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+          rotation={nodes.poleLightB.rotation}
+          scale={nodes.poleLightB.scale}
+        ></mesh>
+
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+          scale={nodes.portalLight.scale}
+        ></mesh>
+      </Center>
     </>
   );
 }
