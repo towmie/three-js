204adf891507c10b42119b37e1134b5327b603d9
towmie <andre00toz@gmail.com>
2024-08-06 15:23:02 +0300

    fireworks: create sphyrical fwrks with different size and radius

--- 27-fireworks/src/script.js
+++ 27-fireworks/src/script.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
 import vertex from "./shader/vertex.glsl";
 import fragment from "./shader/fragment.glsl";
+import gsap from "gsap";
 
 /**
  * Base
@@ -32,6 +33,8 @@ const params = {
   position: new THREE.Vector3(),
   size: 0.5,
   pixelRatio: Math.min(window.devicePixelRatio, 2),
+  radius: 1,
+  color: new THREE.Color("#8affff"),
 };
 params.resolution = new THREE.Vector2(
   sizes.width * params.pixelRatio,
@@ -97,16 +100,25 @@ const texture = [
   textureLoader.load("./particles/8.png"),
 ];
 
-const createFireworks = (count, position, size, texture) => {
+const createFireworks = (count, position, size, texture, radius, color) => {
   const positionArr = new Float32Array(count * 3);
   const sizesArr = new Float32Array(count);
 
   for (let i = 0; i < count; i++) {
     const i3 = i * 3;
 
-    positionArr[i3] = Math.random() - 0.5;
-    positionArr[i3 + 1] = Math.random() - 0.5;
-    positionArr[i3 + 2] = Math.random() - 0.5;
+    const spherical = new THREE.Spherical(
+      radius * (0.75 + Math.random() * 0.25),
+      Math.random() * Math.PI,
+      Math.random() * Math.PI * 2
+    );
+
+    const position = new THREE.Vector3();
+    position.setFromSpherical(spherical);
+
+    positionArr[i3] = position.x;
+    positionArr[i3 + 1] = position.y;
+    positionArr[i3 + 2] = position.z;
 
     sizesArr[i] = Math.random();
   }
@@ -129,14 +141,39 @@ const createFireworks = (count, position, size, texture) => {
       uSize: new THREE.Uniform(size),
       uResolution: new THREE.Uniform(params.resolution),
       uTexture: new THREE.Uniform(texture),
+      uColor: { value: color },
+      uProgress: new THREE.Uniform(0),
     },
   });
 
   const fireWork = new THREE.Points(geometry, material);
   fireWork.position.copy(position);
   scene.add(fireWork);
+
+  const destroy = () => {
+    scene.remove(fireWork);
+    geometry.dispose();
+    material.dispose();
+  };
+
+  gsap.to(material.uniforms.uProgress, {
+    value: 1,
+    duration: 3,
+    ease: "linear",
+    onComplete: destroy,
+  });
 };
-createFireworks(params.count, params.position, params.size, texture[7]);
+
+window.addEventListener("click", () => {
+  createFireworks(
+    params.count,
+    params.position,
+    params.size,
+    texture[7],
+    params.radius,
+    params.color
+  );
+});
 
 /**
  * Animate
--- 27-fireworks/src/shader/fragment.glsl
+++ 27-fireworks/src/shader/fragment.glsl
@@ -1,9 +1,10 @@
 uniform sampler2D uTexture;
+uniform vec3 uColor;
 
 void main(){
     float textureAlpha = texture(uTexture, gl_PointCoord).r;
 
-    gl_FragColor = vec4(1.0, 1.0, 1.0, textureAlpha);
+    gl_FragColor = vec4(uColor, textureAlpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 
