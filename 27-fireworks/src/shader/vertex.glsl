a4bf7a063c75b1e982922c96205044299906a3e7
towmie <andre00toz@gmail.com>
2024-08-06 15:33:46 +0300

    fireworks: explosion

--- 27-fireworks/src/shader/vertex.glsl
+++ 27-fireworks/src/shader/vertex.glsl
@@ -1,10 +1,24 @@
 uniform float uSize;
 uniform vec2 uResolution;
+uniform float uProgress;
 
 attribute float aSize;
 
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
+
+
 void main(){
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec3 newPosition = position;
+
+    float explosion = remap(uProgress, 0.0, 0.1, 0.0, 1.0);
+    explosion = clamp(explosion, 0.0, 1.0);
+    explosion = 1.0 - (1.0 - pow(explosion, 3.0));
+    newPosition *= explosion;
+
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
