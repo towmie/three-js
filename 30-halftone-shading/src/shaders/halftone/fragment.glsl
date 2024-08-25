aedecef2a79e92cacc95944a9d6750ce10fa7f65
towmie <andre00toz@gmail.com>
2024-08-25 14:37:01 +0300

    30: add point grid

--- 30-halftone-shading/src/shaders/halftone/fragment.glsl
+++ 30-halftone-shading/src/shaders/halftone/fragment.glsl
@@ -1,5 +1,5 @@
 uniform vec3 uColor;
-
+uniform vec2 uResolution;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
@@ -11,6 +11,13 @@ void main()
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = uColor;
+    float repetition = 50.0;
+    vec3 direction = vec3(0.0, -1.0, 0.0);
+    float low = -0.8;
+    float high = 1.5;
+
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
 
     vec3 light = vec3(0.0);
 
@@ -25,11 +32,19 @@ void main()
         viewDirection,
         1.0
     );
-    
+
     color *= light;
 
+
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y ;
+    uv *= repetition;
+    uv = mod(uv, 1.0);
+
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity , point);
     // Final color
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(point, point,point,  1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
