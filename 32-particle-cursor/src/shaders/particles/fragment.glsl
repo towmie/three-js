8de4b81979b6a072e82011300d5955324b23daf1
towmie <andre00toz@gmail.com>
2024-08-28 15:45:13 +0300

    32: add dots to texture

---
+++ 32-particle-cursor/src/shaders/particles/fragment.glsl
@@ -0,0 +1,16 @@
+varying vec3 vColor;
+
+void main()
+{
+    // Calculate UV coordinates for the point
+    vec2 uv = gl_PointCoord;
+    float distanceToCenter = distance(uv, vec2(0.5));
+
+    if (distanceToCenter > 0.5) {
+        discard;
+    }
+
+    gl_FragColor = vec4(vColor, 1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
